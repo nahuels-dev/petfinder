0c44b092fe4e4f4cc2c7d11a14b4f3e068520187
Gustavo <75509324+guskpo20@users.noreply.github.com>
2024-04-10 16:33:20 -0300

    responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from "./Navbar.module.scss";
 import Link from 'next/link';
 
 function Navbar() {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("algo")
+    setIsActive(!isActive);
+  };
+
   return (
     <nav className={`${styles.navbar}`}>
-        <div className={`${styles.navbar_container}`}>
+        <div className={`${styles.navbar_container}  ${isActive ? styles.isActive : ''}`}>
             <div className={`${styles.navbar_container_logo}`}></div>
             <div className={`${styles.navbar_container_linksandbtn}`}>
                 <ul>
@@ -16,6 +25,11 @@ function Navbar() {
                     <li><Link href="algo.com">Como funciona?</Link></li>
                 </ul>
                 <button>Iniciar sesion</button>
+            </div>
+            <div className={`${styles.navbar_container_close} ${isActive ? styles.isActive : ''}`} onClick={toggleMenu}>
+              <span className={`${styles.navbar_container_close_line}`}></span>
+              <span className={`${styles.navbar_container_close_line}`}></span>
+              <span className={`${styles.navbar_container_close_line}`}></span>
             </div>
         </div>
     </nav>
