62431b5d3532c056800d2628b95e879580b9a5c8
Nahuel Silva <>
2024-04-19 21:02:23 -0300

    fix login

--- src/components/LogForm/LoginForm.tsx
+++ src/components/LogForm/LoginForm.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 function LoginForm() {
+    useEffect(()=>{
 
-    const sign_in_btn = document.querySelector("#sign-in-btn");
-    const sign_up_btn = document.querySelector("#sign-up-btn");
-    const container = document.querySelector(".container");
-
-    sign_up_btn?.addEventListener("click", () => {
-    container?.classList.add("sign-up-mode");
-    });
-
-    sign_in_btn?.addEventListener("click", () => {
-    container?.classList.remove("sign-up-mode");
-    });
+        const sign_in_btn = document.querySelector("#sign-in-btn");
+        const sign_up_btn = document.querySelector("#sign-up-btn");
+        const container = document.querySelector(".container");
+        
+        sign_up_btn?.addEventListener("click", () => {
+            container?.classList.add("sign-up-mode");
+        });
+        
+        sign_in_btn?.addEventListener("click", () => {
+            container?.classList.remove("sign-up-mode");
+        });
+    },[])
 
   return (
     <div className='contenedorForm'>
