407268a36fd96983b6bf8d35deeadd7bc9190f6a
Gustavo <75509324+guskpo20@users.noreply.github.com>
2024-05-08 11:42:27 -0300

    responsive form

--- src/components/LogForm/LoginForm.tsx
+++ src/components/LogForm/LoginForm.tsx
@@ -1,6 +1,12 @@
 "use client"
 import React, {useEffect, useState} from 'react'
 import styles from './LoginForm.module.scss'
+
+import LoginHuellaIMG from "@/../public/loginhuella.png"
+import LoginAnimalIMG from "@/../public/loginAnimal.png"
+import Image from 'next/image'
+import { Button } from '../Button'
+
 function LoginForm() {
     useEffect(()=>{
 
@@ -16,13 +22,20 @@ function LoginForm() {
             container?.classList.remove("sign-up-mode");
         });
     },[])
-    const [isLogin, setIsLogin] = useState(true)
+    const [isLogin, setIsLogin] = useState(false)
 
+    const RegistrarUsuario = (e:any) =>{
+        console.log("Register")
+    }
 
+    const IniciarSesion =()=>{
+        console.log("Login")
+    }
   return (
     <div className={styles.formcontainer}>
-          <form action="" className={styles.formcontainer__signup}>
-        <h2>Sign Up</h2>
+        <Image src={LoginHuellaIMG} width={673} height={711} alt='huella' className={`${styles.huella} ${isLogin ? styles.loginPosition : ""}`} />
+        <form action={(e) => RegistrarUsuario(e)} className={`${styles.formcontainer__signup} ${isLogin ? styles.movileActive: ""}`}>
+            <h2>Registrarse</h2>
             <label htmlFor="email">
                 <input type="email" placeholder='email@domain.com' id='email' />
             </label>
@@ -30,35 +43,38 @@ function LoginForm() {
                 <input type="password" placeholder='************' id='password' />
             </label>
             <label htmlFor="repeatpassword">
-                <input type="password" placeholder='Confirm password' id='repeatpassword' />
+                <input type="password" placeholder='Repite la contraseña' id='repeatpassword' />
             </label>
-
-            <input type="submit" value='Sign up' />
+    
+            <Button theme='light' >Registrarse</Button>
+            <p onClick={()=>setIsLogin(!isLogin)} className={styles.onlyMovile}>Ya tienes cuenta?</p>
         </form>
 
-        <form action="" className={styles.formcontainer__signin}>
-            <h2>Sign In</h2>
+        <form action={IniciarSesion} className={`${styles.formcontainer__signin} ${isLogin ? "" : styles.movileActive}`}>
+            <h2>Iniciar Sesión</h2>
             <label htmlFor="email">
-                <input type="email" placeholder='email@domain.com' id='email' />
+                <input type="email" placeholder='email@domain.com' id='emailLogin' />
             </label>
             <label htmlFor="password">
-                <input type="password" placeholder='************' id='password' />
+                <input type="password" placeholder='************' id='passwordLogin' />
             </label>
 
-            <input type="submit" value='Login' />
+            <Button theme='light'>Iniciar Sesion</Button>
+            <p onClick={()=>setIsLogin(!isLogin)} className={styles.onlyMovile}>Aun no tienes cuenta?</p>
         </form>
 
         <div className={`${styles.formcontainer__animation} ${isLogin ? styles.formcontainer__animation__login : styles.formcontainer__animation__signup }`}>
+            <Image src={LoginHuellaIMG} width={673} height={711} alt='huella' className={`${styles.huella} ${isLogin ? styles.loginPosition : ""}`} />
             <div className={styles.formcontainer__animation__text}>
-            <h2>No tienes cuenta?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, reprehenderit!</p>
-            <button onClick={()=>setIsLogin(!isLogin)}>Toggle</button>
+                <h2>No tienes cuenta?</h2>
+                <Image src={LoginAnimalIMG} width={656} height={512} alt='Login Animal' />
+                <Button theme='white_bg' onClick={()=>setIsLogin(!isLogin)}>Registrarse</Button>
             </div>
            
             <div className={styles.formcontainer__animation__text}>
-            <h2>Ya tienes una cuenta?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, reprehenderit!</p>
-            <button onClick={()=>setIsLogin(!isLogin)}>Toggle</button>
+                <h2>Ya tienes una cuenta?</h2>
+                <Image src={LoginAnimalIMG} width={656} height={512} alt='Login Animal' />
+                <Button theme='white_bg' onClick={()=>setIsLogin(!isLogin)}>Iniciar Sesión</Button>
             </div>
         </div>
     </div>
