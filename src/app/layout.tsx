61f90e7aa8f5a9aec2a19d5811b906616416aea2
Gustavo <75509324+guskpo20@users.noreply.github.com>
2024-05-11 18:40:02 -0300

    toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+//"use client"
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "@/styles/global.scss";
@@ -6,6 +7,10 @@ import { DialogProvider } from "@/context/Dialog";
 import Footer from "@/components/Footer/Footer";
 const rubik = Rubik({ subsets: ["latin"] });
 
+import Toast from "@/components/Toast/Toast";
+//import { useEffect } from "react";
+
+//Descomentar linea 1, 11 y 32 y comentar Metadata para verlos andando
 export const metadata: Metadata = {
   title: "Pet Finder",
   description: "Encuentra a tu compañero peludo perdido o adopta a un amigo fiel",
@@ -16,6 +21,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const showToast = () => {
+    Toast.fire({
+      icon: 'success',
+      title: '¡Operación exitosa!'
+    });
+  };
+
+  /* useEffect(() => {
+    showToast()
+  }, []) */
+
   return (
     <html lang="en">
       <body className={rubik.className}>
---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,40 @@
+"use client"
+import Swal from 'sweetalert2'
+import styles from './Toast.module.scss'
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+  })
+  
+  ;(async () => {
+    await Toast.fire({
+      icon: 'success',
+      title: 'Success',
+    })
+    await Toast.fire({
+      icon: 'error',
+      title: 'Error',
+    })
+    await Toast.fire({
+      icon: 'warning',
+      title: 'Warning',
+    })
+    await Toast.fire({
+      icon: 'info',
+      title: 'Info',
+    })
+    await Toast.fire({
+      icon: 'question',
+      title: 'Question',
+    })
+  })()
+
+export default Toast
