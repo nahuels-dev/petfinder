439eb087609f67d4e099a92b62c63e12100d3fa4
Nahuel Silva <74438295+nahuels-dev@users.noreply.github.com>
2024-06-06 12:16:18 -0300

    text on live site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/pages/Home.module.scss'
 export default function Home() {
   return (
     <main>
-     <h1>App</h1>
+     <h1>Este sitio esta en construcción</h1>
     </main>
   );
 }
